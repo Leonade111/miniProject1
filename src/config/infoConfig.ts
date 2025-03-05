435b53f3fa293ca2377fe7b7f246f95e1724ff6c
Leo <202660138@stu.scls-sh.org>
2025-03-05 16:26:28 +0800

    Editing

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -3,10 +3,10 @@ export * from './education'
 export * from './career'
 
 // personal info
-export const name = 'Derek Hu'
-export const headline = 'Teacher at Shanghai Soong Ching Ling School.'
+export const name = 'Leo Lin'
+export const headline = 'Student'
 export const introduction =
-  "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020."
+  "Coucou Les Amis! I am a real student!"
 export const email = 'tong.hu@scls-sh.org'
 export const githubUsername = 'derekhut'
 
